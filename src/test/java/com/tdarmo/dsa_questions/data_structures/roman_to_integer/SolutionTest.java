fd1b437d0cd42f1c7e47ea5676f0614bd3ce5c1a
TjandraD <tjandra.darmo@ajaib.co.id>
2022-02-19 12:16:06 +0700

    algo: Improve Roman to Integer solution's time complexity

--- src/test/java/com/tdarmo/dsa_questions/data_structures/roman_to_integer/SolutionTest.java
+++ src/test/java/com/tdarmo/dsa_questions/data_structures/roman_to_integer/SolutionTest.java
@@ -3,87 +3,47 @@ package com.tdarmo.dsa_questions.data_structures.roman_to_integer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Objects;
-
 public class SolutionTest {
     public static class Solution {
-        HashMap<String, Integer> ROMAN_LIST = new HashMap<>();
-
-        public Solution() {
-            ROMAN_LIST.put("I", 1);
-            ROMAN_LIST.put("V", 5);
-            ROMAN_LIST.put("X", 10);
-            ROMAN_LIST.put("L", 50);
-            ROMAN_LIST.put("C", 100);
-            ROMAN_LIST.put("D", 500);
-            ROMAN_LIST.put("M", 1000);
-        }
-
-        public int romanToInt(String s) {
-            String[] inputArray = s.split("");
-
-            int i = 0;
-            for (int j = 0; j < inputArray.length; j++) {
-                String roman = inputArray[j];
-
-                if (Objects.equals(roman, "I") && j != inputArray.length - 1) {
-                    String next = inputArray[j + 1];
-                    if (Objects.equals(next, "V")) {
-                        i += 4;
-                        j++;
-                        continue;
-                    } else if (Objects.equals(next, "X")) {
-                        i += 9;
-                        j++;
-                        continue;
-                    }
-                } else if (Objects.equals(roman, "X") && j != inputArray.length - 1) {
-                    String next = inputArray[j + 1];
-                    if (Objects.equals(next, "L")) {
-                        i += 40;
-                        j++;
-                        continue;
-                    } else if (Objects.equals(next, "C")) {
-                        i += 90;
-                        j++;
-                        continue;
-                    }
-                } else if (Objects.equals(roman, "C") && j != inputArray.length - 1) {
-                    String next = inputArray[j + 1];
-                    if (Objects.equals(next, "D")) {
-                        i += 400;
-                        j++;
-                        continue;
-                    } else if (Objects.equals(next, "M")) {
-                        i += 900;
-                        j++;
-                        continue;
-                    }
+        public static int romanToInt(String s) {
+            int total = 0, current = 0;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                switch (s.charAt(i)) {
+                    case 'I': current = 1; break;
+                    case 'V': current = 5; break;
+                    case 'X': current = 10; break;
+                    case 'L': current = 50; break;
+                    case 'C': current = 100; break;
+                    case 'D': current = 500; break;
+                    case 'M': current = 1000; break;
                 }
 
-                i += ROMAN_LIST.get(roman);
+                // Check if current roman number is less than the next roman number
+                // The roman number usually comes in 5 times bigger than before
+                // So we could use * 2 - 4 to check both numbers
+                if (current * 4 < total) {
+                    total -= current;
+                } else {
+                    total += current;
+                }
             }
 
-            return i;
+            return total;
         }
     }
 
     @Test
     void testRomanToInt_givenFirstExample_returnCorrectInt() {
-        Solution solution = new Solution();
-        Assertions.assertEquals(3, solution.romanToInt("III"));
+        Assertions.assertEquals(3, Solution.romanToInt("III"));
     }
 
     @Test
     void testRomanToInt_givenSecondExample_returnCorrectInt() {
-        Solution solution = new Solution();
-        Assertions.assertEquals(58, solution.romanToInt("LVIII"));
+        Assertions.assertEquals(58, Solution.romanToInt("LVIII"));
     }
 
     @Test
     void testRomanToInt_givenThirdExample_returnCorrectInt() {
-        Solution solution = new Solution();
-        Assertions.assertEquals(1994, solution.romanToInt("MCMXCIV"));
+        Assertions.assertEquals(1994, Solution.romanToInt("MCMXCIV"));
     }
 }
